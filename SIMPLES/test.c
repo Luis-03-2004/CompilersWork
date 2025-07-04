f86022b60baa4b5cd922fb568f167030b372e450
luis-03-2004 <luis.monteiro@alunos.ifsuldeminas.edu.br>
2025-07-04 11:48:08 -0300

    trabalho finalizado

--- SIMPLES/test.c
+++ SIMPLES/test.c
@@ -3,7 +3,7 @@ int fatorial(int n){
    if(n<0){   
       return 0;
    }
-   r=fatorial(n-1);
+   r=   fatorial(n-1);
    return n*r;
 }
 int soma(int n){
@@ -16,7 +16,7 @@ int soma(int n){
    }
    else{   
       return fatorial(n);
-      r=fatorial(n-1);
+      r=      fatorial(n-1);
       return n*r;
    }
 }
@@ -27,9 +27,55 @@ void imprime(int n){
 #include <stdlib.h>
 
 int main(){
-   int numero;
-   int resultado;
-   scanf("%d",&numero);
-   resultado=fatorial(numero);
-   printf("\n%d",resultado);
+   int a;
+   int b=5;
+   int c;
+   int f;
+   int i;
+   float d;
+   int vteste[10];
+   int cteste[10];
+   int dteste[5+8];
+   int r[10];
+   int va[10];
+   int vb[10];
+   float ff[3];
+   cteste[0] = 5;
+   vteste[6] = vteste[4]+cteste[2]-8;
+   for(i = 1;i < 10;i += 1){   
+      scanf("%d",&cteste[i]);
+   }
+   b=8+va[2];
+   scanf("%d",&a);
+   scanf("%d",&c);
+   scanf("%d",&f);
+   scanf("%f",&d);
+   b=(a+b)*c/c+va[2];
+   f=5+2;
+   c=c+f*5;
+   printf("\n%d",b);
+   printf("\n%d",f);
+   printf("\n%d",c);
+   printf("\n%f",d);
+   if(b>f){   
+      printf("\n%d",b);
+   }
+   else{   
+      printf("\n%d",f);
+   }
+   if(a==5){   
+      if(b>5){      
+         if(c<10){         
+            printf("\n%d",a+b+c);
+         }
+      }
+   }
+   while((b>f&&a<1000)||a<f){   
+      printf("\n%d",f*10);
+      f=f-1;
+   }
+   for(i = 10;i > 1;i += -1){   
+      printf("\n%d",i);
+   }
+   b=   fatorial(5);
 }
